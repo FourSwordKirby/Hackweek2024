508d3a00858b249b9bd54b597f986db5019a66bf
Shelby <shelbyjuno@gmail.com>
2025-06-02 15:20:51 -0700

    fix UI

--- MysteryProject/Assets/GameUI.cs
+++ MysteryProject/Assets/GameUI.cs
@@ -10,15 +10,15 @@ public class GameUI : MonoBehaviour
 
     void Update()
     {
-        piles[0].SetText(cardManager.UserBins[0].ToString());
+        piles[0].SetText($"{cardManager.UserBins[0].criteria}\n{cardManager.UserBins[0].pile}");
         piles[0].color = cardManager.currentSelectedPileIndex == 0 ? Color.yellow : Color.white;
 
-        piles[1].SetText(cardManager.UserBins[1].ToString());
+        piles[1].SetText($"{cardManager.UserBins[1].criteria}\n{cardManager.UserBins[1].pile}");
         piles[1].color = cardManager.currentSelectedPileIndex == 1 ? Color.yellow : Color.white;
 
-        piles[2].SetText(cardManager.UserBins[2].ToString());
+        piles[2].SetText($"{cardManager.UserBins[2].criteria}\n{cardManager.UserBins[2].pile}");
         piles[2].color = cardManager.currentSelectedPileIndex == 2 ? Color.yellow : Color.white;
 
-        currentCard.SetText(cardManager.MainDeck.IsEmpty() ? "No cards left" : cardManager.MainDeck.PeekTopCard().ToString());
+        currentCard.SetText(cardManager.MainDeck.IsEmpty() ? "No cards left" : $"<color={cardManager.MainDeck.PeekTopCard().suit}>{cardManager.MainDeck.PeekTopCard()}</color>");
     }
 }
