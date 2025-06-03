db8e4eb0b8d37dc2fb74e13764133293b9b9af55
Shelby <shelbyjuno@gmail.com>
2025-06-03 15:00:15 -0700

    Physics

--- MysteryProject/Assets/GameManager.cs
+++ MysteryProject/Assets/GameManager.cs
@@ -56,7 +56,7 @@ public class GameManager : MonoBehaviour
                     CurrentState = GameManagerState.Running;
                     OnGameStarted?.Invoke();
                 }
-                break;
+                return;
             case GameManagerState.Running:
                 if (PauseButton.wasPressedThisFrame)
                 {
