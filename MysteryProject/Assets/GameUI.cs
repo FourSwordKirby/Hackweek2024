0d2c194b060da48f923fc2674cf99a1c349a3eab
FourSwordKirby <roger.liu@unity3d.com>
2025-06-04 13:59:35 -0700

    added better criteria descriptors

--- MysteryProject/Assets/GameUI.cs
+++ MysteryProject/Assets/GameUI.cs
@@ -33,13 +33,13 @@ public class GameUI : MonoBehaviour
             timeRemaining.SetText(GameManager.instance.TimeRemaining.ToString("00.00"));
         }
 
-        piles[0].SetText($"{cardManager.UserBins[0].criteria}\n{cardManager.UserBins[0].pile}");
+        piles[0].SetText($"{cardManager.UserBins[0].criteria.CriteriaDescriptor()}\n{cardManager.UserBins[0].pile}");
         piles[0].color = cardManager.currentSelectedPileIndex == 0 ? Color.yellow : Color.white;
 
-        piles[1].SetText($"{cardManager.UserBins[1].criteria}\n{cardManager.UserBins[1].pile}");
+        piles[1].SetText($"{cardManager.UserBins[1].criteria.CriteriaDescriptor()}\n{cardManager.UserBins[1].pile}");
         piles[1].color = cardManager.currentSelectedPileIndex == 1 ? Color.yellow : Color.white;
 
-        piles[2].SetText($"{cardManager.UserBins[2].criteria}\n{cardManager.UserBins[2].pile}");
+        piles[2].SetText($"{cardManager.UserBins[2].criteria.CriteriaDescriptor()}\n{cardManager.UserBins[2].pile}");
         piles[2].color = cardManager.currentSelectedPileIndex == 2 ? Color.yellow : Color.white;
 
         currentCard.SetText(cardManager.MainDeck.IsEmpty() ? "No cards left" : $"<color={cardManager.MainDeck.PeekTopCard().suit}>{cardManager.MainDeck.PeekTopCard()}</color>");
